60329be6b9f99d559e0bcd80e97ed99beeba7dfc
diazvergaraarley <diazvergara.arley@gmail.com>
2026-04-28 19:04:46 -0500

    update

--- Prueba_de_desarrollo_m-2/assets/js/main.js
+++ Prueba_de_desarrollo_m-2/assets/js/main.js
@@ -1,7 +1,3 @@
-const date = new Date()
-document.getElementById("year").innerText = date.getFullYear()
-
-// Write your Js code here 
 
 const toggle = document.querySelector(".menu-toggle");
 const nav = document.querySelector("nav");
